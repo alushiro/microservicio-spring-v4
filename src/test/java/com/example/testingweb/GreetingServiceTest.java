5454c8917f6d1415c000f00136251bab41e0f8e4
Eduardo Avello <eduardo.avello.b@gmail.com>
2023-05-16 12:30:31 -0400

    Test

---
+++ src/test/java/com/example/testingweb/GreetingServiceTest.java
@@ -0,0 +1,29 @@
+package com.example.testingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class GreetingServiceTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private GreetingService greetingService;
+
+    @Test
+    public void greetShouldReturnDefaultMessage() throws Exception {
+        String greeting = greetingService.greet();
+        assertThat(greeting).isEqualTo("Finaliza el curso DevOps Foundation v4 - Eduardo Avello");
+    }
+}
