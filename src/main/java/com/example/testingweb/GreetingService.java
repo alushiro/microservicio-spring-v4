2ce2bbccbfd545c5ef010920d7e618555ead52a3
Eduardo Avello <eduardo.avello.b@gmail.com>
2023-05-17 18:34:41 -0400

    test java

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -7,4 +7,12 @@ public class GreetingService {
 	public String greet() {
 		return "Finaliza el curso DevOps Foundation v4 - Eduardo Avello";
 	}
+
+	public boolean validarNumero(int numero) {
+		// Incidencia de bloqueador: No se está realizando la validación del número
+		if (true) {
+			// Código que no realiza la validación correctamente
+		}
+		return true;
+	}
 }
