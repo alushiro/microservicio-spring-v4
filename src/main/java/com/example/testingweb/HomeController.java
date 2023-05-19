30273d84680542b2ee56d1d09e4cb5124f331853
Eduardo Avello <eduardo.avello.b@gmail.com>
2023-05-19 10:22:54 -0400

    Broke2?

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -9,19 +9,6 @@ public class HomeController {
 
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
-		//1-División entre cero, lo cual provoca una excepción ArithmeticException
-		int dividendo = 10;
-		int divisor = 0;
-		int resultado = dividendo / divisor;
-
-//2-Intento de acceder a un índice que está fuera del rango del arreglo, lo cual provoca una excepción ArrayIndexOutOfBoundsException
-		int[] numeros = {1, 2, 3};
-		int indiceInvalido = 5;
-		int numeroInvalido = numeros[indiceInvalido];
-
-//3-Intento de convertir una cadena que no representa un número válido a un tipo entero, lo cual provoca una excepción NumberFormatException
-		String texto = "ABC";
-		int numero = Integer.parseInt(texto);
 		return "Hello, World";
 	}
 
